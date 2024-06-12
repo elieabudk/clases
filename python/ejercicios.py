207d8ca7e9494a6a2f7ec20db7f9ec0516e815b0
Alejandro Chapi <83922158+alejandrochapi94@users.noreply.github.com>
2024-06-12 10:55:59 -0500

    Ejercicios subidos en carpeta python
    Se han subido 8 ejercicios en el documento python para la práctica de la lógica de programación.
    Resolver los ejercicios dentro del mismo documento y hacer un commit señalando que han sido resueltos todos o cuál no ha sido resuelto.

--- python/ejercicios.py
+++ python/ejercicios.py
@@ -1,12 +1,68 @@
-
 #Ejercicio 1
+'''
+ Búsqueda de elementos en una matriz:
+Crea una matriz (lista de listas) de números.
+Escribe una función que busque un número específico en la matriz y devuelva su posición (fila, columna) si se encuentra, o None si no se encuentra.
+Utiliza bucles anidados para recorrer la matriz y comprobar cada elemento.
+Ejemplo:
+Matriz: [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Número a buscar: 5
+Salida: (1, 1)
+'''
 
+#Ejercicio 2
+'''
+Eliminación de elementos duplicados de una lista:
+Crea una lista que contenga elementos duplicados.
+Escribe una función que elimine los elementos duplicados de la lista y devuelva una nueva lista sin duplicados.
+Utiliza un bucle anidado y una tupla para comparar cada elemento con los demás.
+Ejemplo:
+Lista: [1, 2, 2, 3, 4, 4, 5]
+Salida: [1, 2, 3, 4, 5]
+'''
+#Ejercicio 3
+'''
+Imprime un patrón de espiral:
+Escribe un programa que imprima un patrón de espiral de números, dado un número entero 'n' como entrada.
+Utiliza bucles anidados para recorrer la espiral y un diccionario para almacenar las coordenadas de cada número.
+Ejemplo:
+Entrada: 5
+Salida:
+1 2 3 4
+12 13 14 5
+11 16 15 6
+10 9 8 7
+'''
+#Ejercicio 4
+'''
+Crea un juego de adivinar el número:
+Escribe un programa que genere un número aleatorio entre 1 y 100.
+El usuario debe intentar adivinar el número.
+El programa debe proporcionar pistas como "más alto" o "más bajo" hasta que el usuario adivine correctamente.
+Utiliza un bucle while para continuar pidiendo al usuario un número hasta que adivine correctamente.
+'''
+
+#Ejercicio 5
+'''
+Implementar un algoritmo de búsqueda en profundidad (DFS) para encontrar un camino en un laberinto.
+Representar el laberinto como una matriz o un grafo.
+Implementar el algoritmo DFS para recorrer el laberinto y encontrar un camino desde un punto de partida a un punto final.
+Ten en cuenta las restricciones del laberinto y las posibles soluciones.
+'''
 print("hola mundo")
 
 
-#Ejercicio 2
+#Ejercicio 6
+'''
+Implementar un programa en el que se incluya una API de una inteligencia artificial (cualquiera) para tener
+un chatbot que me ayude a practicar inglés.
+'''
 
 
 
-
-#Ejercicio 3
+#Ejercicio 7
+'''
+Agrupación de palabras por longitud:
+Escribe un programa que tome una lista de palabras y las agrupe en un diccionario donde las claves 
+son las longitudes de las palabras y los valores son listas de palabras de esa longitud.
+'''
